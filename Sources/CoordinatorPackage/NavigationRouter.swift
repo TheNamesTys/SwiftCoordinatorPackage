792ca7bf2fc788ac72d8ad6f3e8a6a4c57ba430f
Tyson <44151800+TheNamesTys@users.noreply.github.com>
2023-11-01 13:12:12 +0800

    Update NavigationRouter.swift

--- Sources/CoordinatorPackage/NavigationRouter.swift
+++ Sources/CoordinatorPackage/NavigationRouter.swift
@@ -13,7 +13,7 @@ public protocol NavigationRouter {
     
     associatedtype V: View
 
-    var transition: TransitionStyle { get }
+    var transition: NavigationTransitionStyle { get }
     
     /// Creates and returns a view of assosiated type
     ///
