808de7c276f577f21c89c91032df7c8c706f454b
Tyson <44151800+TheNamesTys@users.noreply.github.com>
2023-11-01 11:39:21 +0800

    renaming to remove 'import helpers'

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "CoordinatorPackage",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(name: "CoordinatorPackage", targets: ["Helpers"])
+        .library(name: "CoordinatorPackage", targets: ["CoordinatorPackage"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -16,7 +16,7 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(name: "Helpers")
+        .target(name: "CoordinatorPackage")
 
     ]
 )
